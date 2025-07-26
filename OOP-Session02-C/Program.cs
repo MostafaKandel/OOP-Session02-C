360734f794b02d7828661d7dc6b5ba3d4f24c337
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-26 05:38:37 +0200

    Add project files.

---
+++ OOP-Session02-C/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Session02_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
