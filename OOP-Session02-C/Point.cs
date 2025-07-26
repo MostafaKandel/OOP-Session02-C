3abcb879ff5b91b079f2927bcfccd3925c6046e5
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-26 06:01:13 +0200

    Q2

---
+++ OOP-Session02-C/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Session02_C
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+
+        // Constructor
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        // Methods
+        public override string ToString()
+        {
+            return $"Point({X}, {Y})";
+        }
+        public static double CaluculateDistance(Point p1, Point p2)
+        {
+            double deltaX = p2.X - p1.X;
+            double deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+    }
+}
--- OOP-Session02-C/Program.cs
+++ OOP-Session02-C/Program.cs
@@ -6,6 +6,7 @@
         {
             #region question 1
             // Person struct
+            /*
             Person[] people = new Person[3];
             people[0] = new Person("MOSTAFA", 30);
             people[1] = new Person("AHMED", 25);
@@ -15,9 +16,27 @@
             {
                 Console.WriteLine(person);
             }
+            */
 
 
             #endregion
+
+            #region question 2
+            // PoINTS
+            Console.Write("please enter X for P1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("please enter Y for P1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("please enter X for P2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("please enter Y for P2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+
+            Console.WriteLine($"the Distance between P1 and P2 is: {Point.CaluculateDistance(p1,p2)}");
+            #endregion
+
         }
     }
 }
