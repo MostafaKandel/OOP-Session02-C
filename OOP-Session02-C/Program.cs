336199e5a21a23dae0d8e30db0bb94f2a639bfe7
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-26 06:13:28 +0200

    Q3

--- OOP-Session02-C/Program.cs
+++ OOP-Session02-C/Program.cs
@@ -23,6 +23,7 @@
 
             #region question 2
             // PoINTS
+            /*
             Console.Write("please enter X for P1: ");
             double x1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("please enter Y for P1: ");
@@ -35,8 +36,43 @@
             Point p2 = new Point(x2, y2);
 
             Console.WriteLine($"the Distance between P1 and P2 is: {Point.CaluculateDistance(p1,p2)}");
+            */
             #endregion
 
+
+            #region question 3
+            // arrray of Persons
+
+            Person[] people = new Person[3];
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.Write($"Enter name for person{i+1}: ");
+                string name = Console.ReadLine();
+                Console.Write($"Enter age for person{i + 1}: ");
+                bool isParsed= int.TryParse(Console.ReadLine(), out int age);
+                while (!isParsed)
+                {
+                    Console.WriteLine("Invalid age input. Please enter a valid integer.");
+                    Console.Write($"Enter age for person {i + 1}: ");
+                    isParsed = int.TryParse(Console.ReadLine(), out age);
+
+                }
+                people[i] = new Person(name, age);
+            }
+
+            Person oldestPerson = people[0];
+            foreach (Person person in people)
+            {
+                if (person.Age > oldestPerson.Age)
+                {
+                    oldestPerson = person;
+                }
+            }
+            Console.Write("the oldest person: ");
+            Console.WriteLine(oldestPerson.ToString());
+
+            #endregion
         }
     }
 }
