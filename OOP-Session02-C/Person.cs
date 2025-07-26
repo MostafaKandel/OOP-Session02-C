dbd80876166288d691ebab677e1b8eaac2a95574
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-26 05:48:13 +0200

    Q1

---
+++ OOP-Session02-C/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Session02_C
+{
+    internal struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // constructor
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
--- OOP-Session02-C/Program.cs
+++ OOP-Session02-C/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question 1
+            // Person struct
+            Person[] people = new Person[3];
+            people[0] = new Person("MOSTAFA", 30);
+            people[1] = new Person("AHMED", 25);
+            people[2] = new Person("MAHMOUD", 35);
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+
+            #endregion
         }
     }
 }
